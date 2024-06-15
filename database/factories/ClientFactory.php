fb01cb5f52847a5100cf792391fa7d1dc6d52682
PauloSilas99 <fonsecasodreps@hotmail.com>
2024-06-15 17:08:41 -0300

    feat: criei a entidade Client

---
+++ database/factories/ClientFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Client>
+ */
+class ClientFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => fake()->numberBetween(1, 9),
+            'name' => fake()->name(),
+            'address' => fake()->address(),
+            'whatsapp' => "99900000000",
+        ];
+    }
+}
