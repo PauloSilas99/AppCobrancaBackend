37391ab07ef57ed59b6ecb1ec8d67dae5461b343
PauloSilas99 <fonsecasodreps@hotmail.com>
2024-06-15 17:30:34 -0300

    feat: rotas feitas

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+// use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::resource('users', UserController::class)->except('create', 'edit');
+Route::resource('clients', ClientController::class)->except('create', 'edit');
+Route::resource('products', ProductController::class)->except('create', 'edit');
